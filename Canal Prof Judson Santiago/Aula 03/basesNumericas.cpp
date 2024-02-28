cc0e198d117204e1675350a3b99921e7e697a9ce
Italo-Thiago <italothiago220@gmail.com>
2024-02-28 16:50:31 -0300

    feat: add Aula 01, 02 folder

---
+++ Canal Prof Judson Santiago/Aula 03/basesNumericas.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Bem Vindo ao C++!" << endl;
+
+	cout << 25 << endl;
+	cout << 0x19 << endl;
+	cout << 031 << endl;
+	cout << 0b11001 << endl;
+}
---
+++ Canal Prof Judson Santiago/Aula 03/constvar.cpp
@@ -0,0 +1,18 @@
+// constvar.cpp - exemplifica o uso de constantes
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "A letra " << 'm';
+  cout << "tem " << 3 << "pernas." << endl;
+
+  char ch = 'M';  // tipo caractere
+  int num = 0x1E; // tipo inteiro
+
+  cout << "Dez letras " << ch;
+  cout << "têm " << num << " pernas." << endl;
+
+  return 0;
+}
+
